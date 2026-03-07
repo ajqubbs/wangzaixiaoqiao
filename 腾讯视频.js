0d81ef858289e14e448cff902bb4250529b5e6d4
ajqubbs <65996571+ajqubbs@users.noreply.github.com>
2026-03-08 01:30:54 +0800

    Update 腾讯视频.js via web editor

--- 腾讯视频.js
+++ 腾讯视频.js
@@ -692,7 +692,7 @@ var rule = {
     play_parse: true,
     lazy: $js.toString(() => {
         try {
-            let api = "http://140.210.9.53:8100/api/?key=e70b5af2f33f0063adad1e4693f5b9ff&url=" + input.split("?")[0];
+            let api = "https://api.jxapi.cc/api/?key=466fb6f21cb027e0f53b7b19baf82c83&url=" + input.split("?")[0];
             console.log(api);
             let response = fetch(api, {
                 method: 'get',
